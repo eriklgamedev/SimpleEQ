82572ea97eef62f512cb0b3b30e5761740cb8fe7
ErikLuoMusic <erik8lrl@gmail.com>
2021-09-03 20:29:20 +0800

    Connected Peak Params

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,6 +10,14 @@
 
 #include <JuceHeader.h>
 
+struct ChainSettings {
+    float peakFreq { 0 }, peakGainInDecibles { 0 }, peakQuality { 1.f };
+    float lowCutFreq { 0 }, highCutFreq { 0 };
+    int lowCutSlope { 0 }, highCutSlope { 0 };
+};
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+
 //==============================================================================
 /**
 */
@@ -65,6 +73,11 @@ private:
 
     MonoChain leftChain, rightChain;
 
+    enum ChainPositions {
+        LowCut,
+        Peak,
+        HighCut
+    };
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
